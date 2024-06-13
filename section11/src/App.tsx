809b3963d761d117a345b13302f9e27672aba88e
임영욱 <ywyim@gaia3d.com>
2024-06-13 15:06:28 +0900

    feat : 리액트와 타입스크립트

---
+++ section11/src/App.tsx
@@ -0,0 +1,90 @@
+import './App.css';
+import React, {
+    useContext,
+    useEffect,
+    useReducer,
+    useRef,
+    useState
+} from 'react';
+import Editor from "./components/Editor";
+import {Todo} from "./types";
+import TodoItem from "./components/TodoItem";
+
+// Action type 정의할 때 union 타입으로
+type Action = {
+    type: 'CREATE',
+    data: {
+        id: number;
+        content: string;
+    }
+} | {
+    type: 'DELETE',
+    id: number;
+};
+
+export const TodoStateContext = React.createContext<Todo[] | null>(null);
+export const TodoDispatchContext = React.createContext<{
+    onClickAdd: (text: string) => void;
+    onClickDelete : (id: number) => void;
+} | null>(null);
+
+export function useTodoDispatch() {
+    const dispatch = useContext(TodoDispatchContext);
+    if(!dispatch) throw new Error('TodoDispatchContext에 문제가 있다.');
+    return dispatch;
+}
+
+function reducer(state: Todo[], action: Action) {
+    switch(action.type) {
+        case 'CREATE':
+            return [...state, action.data];
+        case 'DELETE' :
+            return state.filter(it => it.id !== action.id);
+    }
+
+}
+function App() {
+  const [todos, dispatch] = useReducer(reducer, []);
+  const idRef = useRef(0);
+
+  const onClickAdd = (text: string) => {
+      dispatch({
+          type: 'CREATE',
+          data: {
+              id: idRef.current++,
+              content: text
+          }
+      });
+  }
+
+  const onClickDelete = (id: number) => {
+      dispatch({
+          type: 'DELETE',
+          id: id
+      });
+  }
+
+  useEffect(() => {
+      console.log(todos);
+  }, [todos]);
+
+  return (
+    <div className="App">
+      <h1>Todo</h1>
+        <TodoStateContext.Provider value={todos}>
+            <TodoDispatchContext.Provider
+                value={{
+                    onClickAdd,
+                    onClickDelete
+                }}>
+                <Editor />
+                <div>
+                    {todos.map(todo => <TodoItem key={todo.id} {...todo} />)}
+                </div>
+            </TodoDispatchContext.Provider>
+        </TodoStateContext.Provider>
+    </div>
+  );
+}
+
+export default App;
